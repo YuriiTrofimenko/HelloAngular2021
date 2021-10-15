37acff60cf6c2211000cd6694a5a2cccbd82e36f
YuriiTrofimenko <tyaa@ukr.net>
2021-10-15 20:28:15 +0300

    process selected item id url fragment

--- hello-ng-v2021/src/app/components/pages/to-do-items/to-do-items.component.ts
+++ hello-ng-v2021/src/app/components/pages/to-do-items/to-do-items.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import {ToDoItem} from '../../../models/ToDoItem'
 import {TodoItemsService} from "../../../services/todo-items.service";
 import {CommonService} from "../../../services/common.service"
+import {NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-to-do-items',
@@ -17,14 +18,46 @@ export class ToDoItemsComponent implements OnInit {
   title: string = ''
 
   isLoading: boolean
+  allowUrlProcessing: boolean = true
 
-  constructor(private itemsService: TodoItemsService) {
+  constructor(
+    private router: Router,
+    private itemsService: TodoItemsService
+  ) {
     itemsService.commonService.getIsLoadingStream().subscribe(value => {
       this.isLoading = value
     })
   }
 
+  processUrl () {
+    if (this.allowUrlProcessing) {
+      this.allowUrlProcessing = false
+      const hash = window.location.hash
+      if (hash && this.toDoItems.length > 0) {
+        const selectedItemId = Number(hash.replace('#', ''))
+        const selectedItem =
+          this.toDoItems.find(item => item.id === selectedItemId)
+        if (selectedItem) {
+          this.selectedItem = selectedItem
+        } else {
+          if (!this.selectedItem) {
+            window.location.hash = ''
+          } else {
+            window.location.hash = this.selectedItem.id.toString()
+          }
+        }
+      }
+      this.allowUrlProcessing = true
+    }
+  }
+
   ngOnInit() {
+    this.router.events.forEach(event => {
+      // console.log(event instanceof NavigationStart)
+      if (event instanceof NavigationStart) {
+        this.processUrl()
+      }
+    })
     setTimeout(() => this.title = 'Hello Angular 2021!', 5000)
     // this.toDoItems.push(...this.service.getItems())
     this.itemsService.getRemoteItems()
@@ -38,11 +71,14 @@ export class ToDoItemsComponent implements OnInit {
         },
         error => {
           console.log(error);
+        },
+        () => {
+          this.processUrl()
         });
   }
 
   onItemClick (toDoItem: ToDoItem) {
     this.selectedItem = toDoItem
+    this.router.navigate(['tasks'], { fragment: toDoItem.id.toString() })
   }
-
 }
